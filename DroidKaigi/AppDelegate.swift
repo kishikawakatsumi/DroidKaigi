14b5b6b3d3b6971253f19070a89c8f229f81d7e1
watanave <watanabe@crypton.co.jp>
2017-03-09 13:31:41 +0900

    通知権限取得

--- DroidKaigi/AppDelegate.swift
+++ DroidKaigi/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { (granted, error) in }
+        
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            switch settings.authorizationStatus {
+            case .authorized:
+                print("authorized")
+            case .denied:
+                print("denied")
+            default:
+                print("notDetermined")
+            }
+        }
+        
         return true
     }
 }
