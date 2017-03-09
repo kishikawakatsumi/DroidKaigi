dad5d2cbb683b3ed40e588124e9d35f2a038bfca
watanave <watanabe@crypton.co.jp>
2017-03-10 08:43:59 +0900

    Change NotificationTitle to room number

--- DroidKaigi/SessionView.swift
+++ DroidKaigi/SessionView.swift
@@ -126,10 +126,10 @@ class SessionView: UIView {
             let center = UNUserNotificationCenter.current()
             
             let content = UNMutableNotificationContent()
-            content.title = "Droid Kaigi"
+            content.title = session.room?.name ?? "DroidKaigi"
             content.body = self.session.title
             content.sound = UNNotificationSound.default()
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
             let request = UNNotificationRequest(identifier: session.notificationId, content: content, trigger: trigger)
             center.add(request)
             
