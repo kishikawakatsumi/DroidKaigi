c1c6d624b9cfab700af642c1722fd2c821325de4
Kosuke Ogawa <koogawa.app@gmail.com>
2017-03-10 17:49:23 +0900

    Connect links

--- DroidKaigi/InformationViewController.swift
+++ DroidKaigi/InformationViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import SafariServices
 
 class InformationViewController: UITableViewController {
 
+    private enum Item: Int {
+        case sponsor
+        case enquete
+        case contributer
+        case translate
+        case license
+        case devInfo
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +30,50 @@ class InformationViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Private methods
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapTwitterButton(_ sender: Any) {
+        openUrl(string: "https://twitter.com/DroidKaigi")
     }
-    */
-
+    
+    @IBAction func didTapFacebookButton(_ sender: Any) {
+        openUrl(string: "https://www.facebook.com/DroidKaigi/")
+    }
+    
+    @IBAction func didTapGithubButton(_ sender: Any) {
+        openUrl(string: "https://github.com/DroidKaigi/conference-app-2017/")
+    }
+    
+    @IBAction func didTapDroidKaigiButton(_ sender: Any) {
+        openUrl(string: "https://droidkaigi.github.io/2017/")
+    }
+    
+    @IBAction func didTapYoutubeButton(_ sender: Any) {
+        openUrl(string: "https://www.youtube.com/droidkaigi")
+    }
+    
+    private func openUrl(string: String) {
+        if let url = URL(string: string) {
+            let viewController = SFSafariViewController(url: url)
+            self.present(viewController, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - UITableView delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        switch indexPath.row {
+        case Item.sponsor.rawValue:
+            openUrl(string: "https://droidkaigi.github.io/2017/#sponsors")
+        case Item.enquete.rawValue:
+            openUrl(string: "https://docs.google.com/forms/d/1SNBvJernnyBwglNentXxpdSUkWI9U6umWdDs4Na8OIU/viewform?edit_requested=true")
+        case Item.contributer.rawValue:
+            openUrl(string: "https://github.com/kishikawakatsumi/DroidKaigi/graphs/contributors")
+        case Item.translate.rawValue:
+            openUrl(string: "https://droidkaigi2017.oneskyapp.com/collaboration")
+        default: break
+        }
+    }
 }
